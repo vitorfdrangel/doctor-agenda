ddc9b369d95f715346ad01c9528351e39e47ef04
Vitor Rangel <vitorfdrangel@gmail.com>
2025-10-09 18:17:53 -0300

    feat: add top specialties list

---
+++ src/app/(protected)/dashboard/_components/top-specialties.tsx
@@ -0,0 +1,91 @@
+import {
+  Baby,
+  Bone,
+  Eye,
+  Heart,
+  Hospital,
+  Stethoscope,
+  Venus,
+} from "lucide-react";
+import React from "react";
+
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+
+interface TopSpecialtiesProps {
+  topSpecialties: {
+    specialty: string;
+    appointments: number;
+  }[];
+}
+
+const getSpecialtyIcon = (specialty: string) => {
+  switch (specialty.toLowerCase()) {
+    case "cardiologia":
+      return Heart;
+    case "ortopedia e traumatologia":
+      return Bone;
+    case "ginecologia e obstetrícia":
+      return Venus;
+    case "oftalmologia":
+      return Eye;
+    case "pediatria":
+      return Baby;
+    default:
+      return Stethoscope;
+  }
+};
+
+export default function TopSpecialties({
+  topSpecialties,
+}: TopSpecialtiesProps) {
+  const maxAppointments = Math.max(
+    ...topSpecialties.map((i) => i.appointments),
+  );
+
+  return (
+    <Card className="mx-auto w-full">
+      <CardContent>
+        <div className="mb-8 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <Hospital className="text-muted-foreground" />
+            <CardTitle className="text-base">Especialidades</CardTitle>
+          </div>
+        </div>
+
+        <div className="space-y-6">
+          {topSpecialties.map((specialty) => {
+            const Icon = getSpecialtyIcon(specialty.specialty);
+            const progressValue =
+              (specialty.appointments / maxAppointments) * 100;
+
+            return (
+              <div
+                key={specialty.specialty}
+                className="flex items-center gap-2"
+              >
+                <div className="bg-primary/10 flex h-10 w-10 items-center justify-center rounded-full">
+                  <Icon className="text-primary h-5 w-5" />
+                </div>
+
+                <div className="flex w-full flex-col justify-center">
+                  <div className="flex w-full justify-between">
+                    <h3 className="text-sm">{specialty.specialty}</h3>
+
+                    <div className="text-right">
+                      <span className="text-muted-foreground text-sm font-medium">
+                        {specialty.appointments} agend.
+                      </span>
+                    </div>
+                  </div>
+
+                  <Progress value={progressValue} className="w-full" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -21,6 +21,7 @@ import AppointmentsChart from "./_components/appointments-chart";
 import { DatePicker } from "./_components/date-picker";
 import StatsCards from "./_components/stats-cards";
 import TopDoctors from "./_components/top-doctors";
+import TopSpecialties from "./_components/top-specialties";
 
 interface DashboardPageProps {
   searchParams: Promise<{
@@ -54,6 +55,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     [totalPatients],
     [totalDoctors],
     topDoctors,
+    topSpecialties,
   ] = await Promise.all([
     db
       .select({
@@ -111,6 +113,25 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
       .groupBy(doctorsTable.id)
       .orderBy(desc(count(appointmentsTable.id)))
       .limit(10),
+    db
+      .select({
+        specialty: doctorsTable.specialty,
+        appointments: count(appointmentsTable.id),
+      })
+      .from(doctorsTable)
+      .innerJoin(
+        appointmentsTable,
+        eq(appointmentsTable.doctorId, doctorsTable.id),
+      )
+      .where(
+        and(
+          eq(appointmentsTable.clinicId, session.user.clinicId),
+          gte(appointmentsTable.date, new Date(from)),
+          lte(appointmentsTable.date, new Date(to)),
+        ),
+      )
+      .groupBy(doctorsTable.specialty)
+      .orderBy(desc(count(appointmentsTable.id))),
   ]);
 
   const chartStartDate = dayjs().subtract(10, "days").startOf("day").toDate();
@@ -161,6 +182,11 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
           <div className="grid grid-cols-[2.25fr_1fr] gap-4">
             <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
             <TopDoctors doctors={topDoctors} />
+          </div>
+
+          <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+            tabela
+            <TopSpecialties topSpecialties={topSpecialties} />
           </div>
         </CardContent>
       </PageContent>
